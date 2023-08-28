4a225ac0526c3fd0f38425e30290d8f5e3c14b83
hinyong <97842684+HinYong1031@users.noreply.github.com>
2023-08-28 11:49:54 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cu7xzzvhdibx.us-east-1.rds.amazonaws.com"
+customuser = "awsuser"
+custompass = "hinyong1031"
+customdb = "employee"
+custombucket = "voonhinyong-employee"
+customregion = "us-east-1"
+
